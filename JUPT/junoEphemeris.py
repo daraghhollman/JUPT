f3d2e50c02999c2e86537a792f4426feb793edb1
Daragh Hollman <hollmandaragh@gmail.com>
2024-06-06 20:51:05 +0100

    Removed accidental '}' causing issues with Python 3.12

--- JUPT/junoEphemeris.py
+++ JUPT/junoEphemeris.py
@@ -78,7 +78,7 @@ def PlotEphemeris(ax, dataTime, timeFrame, resolutionFactor=None, labels=True, l
     
     if labels:
         ax.annotate('R (R$_J$)',xy=(0,0), xycoords='axes fraction', xytext=(labelsPos[0], labelsPos[1]-0*labelFontsize), textcoords='offset points', horizontalalignment='right', verticalalignment='center',fontsize=labelFontsize)
-        ax.annotate('Lon$_{III}$ ($^\circ}$)',xy=(0,0), xycoords='axes fraction', xytext=(labelsPos[0], labelsPos[1]-1*labelFontsize - posSpacer), textcoords='offset points', horizontalalignment='right', verticalalignment='center',fontsize=labelFontsize)
+        ax.annotate('Lon$_{III}$ ($^\circ$)',xy=(0,0), xycoords='axes fraction', xytext=(labelsPos[0], labelsPos[1]-1*labelFontsize - posSpacer), textcoords='offset points', horizontalalignment='right', verticalalignment='center',fontsize=labelFontsize)
         ax.annotate('Lat ($^\circ$)',xy=(0,0), xycoords='axes fraction', xytext=(labelsPos[0], labelsPos[1]-2*labelFontsize - 2*posSpacer), textcoords='offset points', horizontalalignment='right', verticalalignment='center',fontsize=labelFontsize)
         ax.annotate('MLat ($^\circ$)',xy=(0,0), xycoords='axes fraction', xytext=(labelsPos[0], labelsPos[1]-3*labelFontsize - 3*posSpacer), textcoords='offset points', horizontalalignment='right', verticalalignment='center',fontsize=labelFontsize)
     else:
